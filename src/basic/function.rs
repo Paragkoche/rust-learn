251623c68dff2b5f80fd4f6d983f9e19a80224a5
paragkoche <paragkoche0@gmail.com>
2025-01-12 19:02:11 +0530

    update

--- src/basic/function.rs
+++ src/basic/function.rs
@@ -1,3 +1,65 @@
 pub fn fun1(name: &str) {
     println!("{} !!", name)
 }
+
+pub fn fibonacci_number(number: i32) -> i32 {
+    if number == 1 || number == 0 {
+        return 1;
+    }
+
+    return fibonacci_number(number - 1) + fibonacci_number(number - 2);
+}
+
+pub fn factorial_number(number: i128) -> i128 {
+    if number == 1 || number == 0 {
+        return 1;
+    }
+    return number * factorial_number(number - 1);
+}
+
+pub fn sort_bobble(array: &[i128]) -> Vec<i128> {
+    let mut array = array.to_vec();
+    for i in 0..array.len() {
+        for j in i + 1..array.len() {
+            if array[i] > array[j] {
+                array.swap(i, j);
+            }
+        }
+    }
+    array
+}
+fn middle_value(array: &mut Vec<i128>, low: usize, high: usize) -> usize {
+    let pivot = array[high];
+    let mut i = low as isize - 1; // Initialize i as isize to handle -1 case
+
+    for j in low..high {
+        if array[j] < pivot {
+            i += 1;
+            array.swap(i as usize, j);
+        }
+    }
+
+    // Place the pivot in the correct position
+    let i = (i + 1) as usize;
+    array.swap(i, high);
+    i
+}
+
+pub fn quick_sort(array: &mut Vec<i128>, low: usize, high: usize) {
+    if low < high {
+        let pi = middle_value(array, low, high);
+
+        if pi > 0 {
+            quick_sort(array, low, pi - 1); // Sort the left part
+        }
+        quick_sort(array, pi + 1, high); // Sort the right part
+    }
+}
+pub fn liner_search(array: Vec<i128>, search_element: i128) -> Result<usize, &'static str> {
+    for i in 0..array.len() {
+        if array[i] == search_element {
+            return Ok(i);
+        }
+    }
+    return Err("element not found");
+}
--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,22 @@ fn main() {
     basic::print::table2(55);
     print!("{esc}c", esc = 27 as char); //that is use to clear screen
     basic::function::fun1("name");
+    println!("fibonacci 10 = {}", basic::function::fibonacci_number(10));
+    println!("factorial 10! = {}", basic::function::factorial_number(10));
+    println!(
+        "array {:?} = {:?}",
+        [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+        basic::function::sort_bobble(&[10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    );
+    let mut array = vec![10, 7, 8, 9, 1, 5];
+    let len = array.len();
+    basic::function::quick_sort(&mut array, 0, len - 1);
+    let sorted_array = array.clone();
+    println!("array {:?} = {:?}", [10, 7, 8, 9, 1, 5], sorted_array);
+
+    println!(
+        "array {:?} search element {:?}",
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        basic::function::liner_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].to_vec(), 10)
+    )
 }
